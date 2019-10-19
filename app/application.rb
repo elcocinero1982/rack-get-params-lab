c04566c40ded75038b00584d3b07edf1c9aef112
jose luis <cortezluisjose@gmail.com>
2019-10-19 02:58:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,4 +26,4 @@ class Application
 			elsif req.path.match(/search/)
       search_term = req.params["q"]	      
       resp.write handle_search(search_term)	       handle_search(search_term)
-   
+   end
